112ccea1e1d31f84b3e958d3d5d1e43a3896758c
Andres Camacho <99270915+af01camacho@users.noreply.github.com>
2023-05-26 17:14:51 -0500

    Navbar-Logo

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,17 @@ import './App.css'
 import Home from './components/home/Home'
 import { Route, Routes, Link } from 'react-router-dom'
 import Services from './components/services/Services'
+import NavBar from './components/navbar/NavBar'
 function App() {
   
 
   return (
     <>
+    <NavBar/>
        <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/services' element={<Services/>} />
         </Routes>
-      
     </>
   )
 }
