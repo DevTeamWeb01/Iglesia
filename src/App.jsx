48279681764dab4a6214e98bde96ef40fe747ea5
Andres Camacho <99270915+af01camacho@users.noreply.github.com>
2023-05-26 16:17:18 -0500

    update router-doom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 
 import './App.css'
-
+import Home from './components/home/Home'
+import { Route, Routes, Link } from 'react-router-dom'
+import Services from './components/services/Services'
 function App() {
   
 
   return (
     <>
+       <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/services' element={<Services/>} />
+        </Routes>
       
     </>
   )
