1aadddf91ebed7b17b3947ef6458fd5abfc1d242
af01camacho <af01camacho@gmail.com>
2023-05-27 21:49:12 -0500

    Menu-mobilefixed

--- src/components/sections/navbar/NavBar.jsx
+++ src/components/sections/navbar/NavBar.jsx
@@ -1,6 +1,17 @@
 import { Link } from "react-router-dom";
 import './NavBar.css'
+import { useState } from "react";
 const NavBar = () => {
+
+    const [activeMenu, setActiveMenu] = useState(false);
+
+    const removeMenu = () => {
+        if(!activeMenu){
+            setActiveMenu(true);
+        }else{
+            setActiveMenu(false)
+        }
+    }
     return (
         <>
             <div className="navbar-info">
@@ -31,26 +42,29 @@ const NavBar = () => {
                     <button className="navbar-btn">Realizar donaciones</button>
                 </div>
                 <div className="navbar-mobile">
-                    <Link className="navbar-mobile-item" to=''> <box-icon size='30px' color='#000' name='menu'></box-icon> </Link>
+                    <Link onClick={removeMenu} className="navbar-mobile-item" to=''> <box-icon size='30px' color='#000' name='menu'></box-icon> </Link>
                 </div>
                 
             </nav>
-            <ul className="navbar-list-mobile">
-                    <Link to='/'  className="navbar-item" > Home </Link>
-                    <Link to='/services' className="navbar-item" > Services
-                        <box-icon color='#fff' name='chevron-down'></box-icon>
-                    </Link>
-                    <Link to='/acerca-de-nosotros' className="navbar-item" > Nosotros
-                        <box-icon color='#fff' name='chevron-down'></box-icon>
-                     </Link>
-                     <Link to='/services' className="navbar-item" > Que hacemos
-                        <box-icon color='#fff' name='chevron-down'></box-icon>
-                     </Link>
-                     <Link to='/services' className="navbar-item" > Recursos
-                        <box-icon color='#fff' name='chevron-down'></box-icon>
-                     </Link>
-                     </ul>
-        </>
+           { activeMenu && (
+             <ul className="navbar-list-mobile">
+             <Link to='/'  className="navbar-item-mobile" > Home </Link>
+             <Link to='/services' className="navbar-item-mobile" > Services
+                 <box-icon color='#fff' name='chevron-down'></box-icon>
+             </Link>
+             <Link to='/acerca-de-nosotros' className="navbar-item-mobile" > Nosotros
+                 <box-icon color='#fff' name='chevron-down'></box-icon>
+              </Link>
+              <Link to='/services' className="navbar-item-mobile" > Que hacemos
+                 <box-icon color='#fff' name='chevron-down'></box-icon>
+              </Link>
+              <Link to='/services' className="navbar-item-mobile" > Recursos
+                 <box-icon color='#fff' name='chevron-down'></box-icon>
+              </Link>
+              </ul>
+           )
+           }
+            </>
      );
 }
 
