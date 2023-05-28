c387eda53ad9302a0517d65ee3c1b44f9732865d
Gabriel Martinez <martinegabriel47@gmail.com>
2023-05-28 12:34:56 -0500

    Create creencias.js

---
+++ src/JS/creencias.js
@@ -0,0 +1,39 @@
+const creencias = [
+    {
+        id: '1',
+        titulo: 'Escritura inspirada por Dios',
+        descripcion: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid, nulla optio fuga ipsum ad ut nesciunt ducimus quaerat est cupiditate, ratione illo. Facilis perspiciatis exercitationem ipsum, tenetur accusamus voluptates iste'
+    },
+
+    {
+        id: 2,
+        titulo: 'Cristo vivió, murió y resucitó',
+        descripcion: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid, nulla optio fuga ipsum ad ut nesciunt ducimus quaerat est cupiditate, ratione illo. Facilis perspiciatis exercitationem ipsum, tenetur accusamus voluptates iste'
+    },
+
+    {
+        id: 3,
+        titulo: 'Él pronto volverá por nosotros',
+        descripcion: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid, nulla optio fuga ipsum ad ut nesciunt ducimus quaerat est cupiditate, ratione illo. Facilis perspiciatis exercitationem ipsum, tenetur accusamus voluptates iste'
+    },
+
+    {
+        id: 4,
+        titulo: 'Debemos ser imitadores de él',
+        descripcion: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid, nulla optio fuga ipsum ad ut nesciunt ducimus quaerat est cupiditate, ratione illo. Facilis perspiciatis exercitationem ipsum, tenetur accusamus voluptates iste'
+    },
+
+    {
+        id: 5,
+        titulo: 'Debemos nacer de nuevo',
+        descripcion: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid, nulla optio fuga ipsum ad ut nesciunt ducimus quaerat est cupiditate, ratione illo. Facilis perspiciatis exercitationem ipsum, tenetur accusamus voluptates iste'
+    },
+
+    {
+        id: 6,
+        titulo: 'Es necesario congregarse',
+        descripcion: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid, nulla optio fuga ipsum ad ut nesciunt ducimus quaerat est cupiditate, ratione illo. Facilis perspiciatis exercitationem ipsum, tenetur accusamus voluptates iste'
+    },
+];
+
+export default creencias;
